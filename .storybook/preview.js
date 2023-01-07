0469f2a77032c5ac6db8b1af960dc79ec8f676c5
RodgerRG <RodgerG@hotmail.co.nz>
2023-01-07 16:56:13 +1300

    Got tailwind working with storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import { DEFAULT_THEME, withTailwindTheme } from './withTailwindTheme.decorator';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,23 @@ export const parameters = {
     },
   },
 }
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    toolbar: {
+      icon: 'paintbrush',
+      // Array of plain string values or MenuItem shape
+      items: [
+        { value: 'light', title: 'Light', left: '🌞' },
+        { value: 'dark', title: 'Dark', left: '🌛' },
+      ],
+      // Change title based on selected value
+      dynamicTitle: true,
+    },
+  },
+};
+
+
+export const decorators = [withTailwindTheme];
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,11 @@ import React, { ComponentProps } from 'react';
 export interface HeaderProps extends ComponentProps<'header'> {}
 
 const Header = ({ children }: HeaderProps): JSX.Element => {
-    return <header className=''>{children}</header>;
+    return (
+        <header className='flex items-center space-x-4 shadow-md flex-row bg-white text-black'>
+            {children}
+        </header>
+    );
 };
 
 export { Header };
